7b3925c9c1f4fd5d4d14aea0ea76d88c06d60fb6
Mariano Moreno Alvariza <batiar@gmail.com>
2017-11-16 13:10:39 +0000

    Agrego jquery, y controller main

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'main/welcome'
+
   resources :tweets
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "tweets#index"
+  root to: "main#welcome"
 end
