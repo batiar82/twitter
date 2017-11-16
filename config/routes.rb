8a3b0c3509aabe0af2e5fb12d2d1723f096ead36
Mariano Moreno Alvariza <batiar@gmail.com>
2017-11-16 12:57:54 +0000

    Agrego Tweets

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :tweets
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "home#index"
+  root to: "tweets#index"
 end
