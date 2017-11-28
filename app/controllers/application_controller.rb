91edaf34a9b1482bb7fcc3c227b9935c337931e7
Mariano Moreno Alvariza <batiar@gmail.com>
2017-11-28 14:22:35 +0000

    modifico application_controller pq el metodo q comente rompia el edit register

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  protected
-  def authenticate_user!
-    if user_signed_in?
-      super
-    else
-      redirect_to login_path
+  #protected
+  #def authenticate_user!
+  #  if user_signed_in?
+  #    super
+  #  else
+  #    redirect_to login_path
       ## if you want render 404 page
       ## render :file => File.join(Rails.root, 'public/404'), :formats => [:html], :status => 404, :layout => false
-    end
-  end
+  #  end
+  #end
 end
